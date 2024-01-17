44bb5d8db1b59592cc12b9b8a2a5f8fe8aacce0d
chanee718 <chanee718@kaist.ac.kr>
2024-01-17 16:24:22 +0900

    main change

--- src/search/Search.tsx
+++ src/search/Search.tsx
@@ -5,7 +5,6 @@ const Search: React.FC<{ searchedMovies: any[] }> = ({ searchedMovies }) => {
     console.log(searchedMovies)
   return (
     <div>
-        <h2>Search Results</h2>
         <SearchResult carouselList={searchedMovies} />
     </div>
   );
